b6a14483a5ff5a996ba36045c5cd4b181c8d50bd
Sergey Gribovski <megacoder@rambler.ru>
2011-12-21 01:49:53 +0400

    change config template

--- lib/tasks/templates/semantic_navigation.rb
+++ lib/tasks/templates/semantic_navigation.rb
@@ -1,26 +1,15 @@
 #This file need to create the navigation in your app.
 SemanticNavigation::Configuration.run do |config|
 
-  #What's the name of the active menu class will be (the dafault is 'active')
-  config.active_class = 'active';
-  
-  #Create the menu ids automatically?
-  config.create_ids = true
-  
-  #Show the submenu only when the menu is active
-  config.show_submenu = true
-  
-  #That's how you can create your userbar menu
-  #config.userbar do |userbar|
-  #  userbar.signin :controller => :session, :action => :signin, :if => current_user.nil?
-  #  userbar.signout :controller => :session, :action => :signout, :if => !current_user.nil?
-  #end
-  #so you can use the helper 'render_userbar_menu' to render it.
-
   #That's the creation of the navigation menu
-  config.navigation #do |navigation|
-    #navigation.first 'first', :controller => :first, :action => :act
-    #navigation.second 'second', :controller => :second, :action => :act
-  #end
+  config.navigation do |n|
+    n.main 'Main', :controller => :dashboard, :action => :index
+    n.about 'About', :controller => :about, :action => :index do |a|
+      a.company 'About company', :controller => :about, :action => :company
+      a.employes 'About our employes', :controller => :about, :action => :employes
+    end
+    n.feed_back 'Feedback', :controller => :feed_back, :action => :index
+  end
+  #You can render this menu by calling method render_navigation_menu
 
 end
