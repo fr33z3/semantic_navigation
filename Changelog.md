e333358a74338b39d4a906f99e7c9ee813aa1d2a
Sergey Gribovski <megacoder@rambler.ru>
2011-12-25 16:39:21 +0400

    Added prefixes definition

--- Changelog.md
+++ Changelog.md
@@ -1,12 +1,13 @@
 ### 0.0.2 / Unknown
 
-* Possibility to define default render options (active_class and show_ids)
-* Possibility to define the private render options to each menu item
-* Possibility to render or not to render submenu if parent item is inactive
-* Catch of undefined helper methods
-* Catch of undefined menu render calls
+* Definition of default render options (active_class and show_ids)
+* Definition of the private render options to each menu item
+* Definition of default option that says render submenu always or only then parent is active
+* Raise error on undefined helper methods calls
+* Raise error on undefined menu render calls
 * Added helper method to render active name submenu
 * Changed helper method to render menu
+* Added prefixes for menu, item, name ids
 
 ### 0.0.1 / December 23th, 2011
 
--- lib/semantic_navigation/item.rb
+++ lib/semantic_navigation/item.rb
@@ -10,9 +10,14 @@ module SemanticNavigation
     @@show_item_id = true
     @@show_name_id = true
     @@show_submenu = false
+    @@menu_prefix = ''
+    @@item_perfix = ''
+    @@name_prefix = ''
     
     attr_accessor :active_class, :show_active_class, :show_menu_id,
-                  :show_item_id, :show_name_id, :show_submenu
+                  :show_item_id, :show_name_id, :show_submenu,
+                  :menu_prefix, :item_prefix, :name_prefix,
+                  :item_classes
     
     def initialize(id, args, parent)
       @item_id = id
@@ -38,24 +43,32 @@ module SemanticNavigation
     private
     
     def ul_id
+      prefix = @menu_prefix.nil? ? @@menu_prefix : @menu_prefix
       flag = @show_menu_id.nil? ? @@show_menu_id : @show_menu_id
-      flag ? @item_id : nil
+      flag ? prefix+@item_id : nil
     end
     
     def li_id
+      prefix = @item_prefix.nil? ? @@item_prefix : @item_prefix
       flag = @show_item_id.nil? ? @@show_item_id : @show_item_id
-      flag ? @item_id : nil
+      flag ? prefix+@item_id : nil
     end
     
     def a_id
+      prefix = @name_prefix.nil? ? @@name_prefix : @name_prefix
       flag = @show_name_id.nil? ? @@show_name_id : @show_name_id
-      flag ? @item_id : nil
+      flag ? prefix+@item_id : nil
     end
     
     def classes
+      class_array = []
+      class_array << item_classes
       active_class = @active_class.nil? ? @@active_class : @active_class
       flag = @show_active_class.nil? ? @@show_active_class : @show_active_class
-      @active && flag ? active_class : nil 
+      if @active && flag
+        class_array << active_class
+      end
+      class_array.flatten.join(' ')
     end
     
     def view_object
