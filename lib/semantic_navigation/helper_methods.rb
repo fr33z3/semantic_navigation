7b32eaa1e141e997723957aa515506a9afc61b16
Sergey Gribovski <megacoder@rambler.ru>
2012-01-11 01:45:57 +0400

    speed up the rendering

--- lib/semantic_navigation/helper_methods.rb
+++ lib/semantic_navigation/helper_methods.rb
@@ -1,18 +1,23 @@
 module SemanticNavigation::HelperMethods
   
-  def initialize
-    @__semantic_navigation_config = eval(IO.read("#{Rails.root}/config/semantic_navigation.rb"))
-    super
-  end
-  
   def method_missing(name, command = :render)
     name = name.to_s
     if name[0..6] == 'render_'
       SemanticNavigation::Item.set_default_option('view_object',self)
-      @__semantic_navigation_config.render(name[7..-1], command)
+      semantic_navigation_config.render(name[7..-1], command)
     else
       raise NoMethodError.new("NoMethodError:#{name}")
     end
   end
+
+  private
+ 
+  def semantic_navigation_config
+    if @__semantic_navigation_config.nil?
+      @__semantic_navigation_config = eval(IO.read("#{Rails.root}/config/semantic_navigation.rb"))
+    end
+    @__semantic_navigation_config
+  end
+ 
 
 end
