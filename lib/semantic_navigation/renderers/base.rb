88e30d110d1966d6bb6eaa53c121e1828e743687
Sergey Gribovski <megacoder@rambler.ru>
2012-03-20 01:10:29 +0400

    added styles and link_to, content_tag methods wrapping

--- lib/semantic_navigation/renderers/base.rb
+++ lib/semantic_navigation/renderers/base.rb
@@ -4,11 +4,34 @@ module SemanticNavigation
     
       attr_accessor :from_level, :until_level
       
-      @@navigation_active_class = 'active'
-      @@node_active_class = 'active'
-      @@leaf_active_class = 'active'
-      @@link_active_class = 'active'
+      cattr_accessor :navigation_active_class, :node_active_class, :leaf_active_class, 
+                     :link_active_class, :show_navigation_active_class,
+                     :show_node_active_class, :show_leaf_active_class, 
+                     :show_link_active_class,
+                     :show_navigation_id, :show_node_id, :show_leaf_id, :show_link_id,
+                     :navigation_default_classes, :leaf_default_classes, 
+                     :node_default_classes, :link_default_classes
       
+      @@navigation_active_class = [:active]
+      @@node_active_class = [:active]
+      @@leaf_active_class = [:active]
+      @@link_active_class = [:active]
+      
+      @@show_navigation_active_class = true
+      @@show_node_active_class = true
+      @@show_leaf_active_class = true
+      @@show_link_active_class = true
+      
+      @@show_navigation_id = true
+      @@show_node_id = true
+      @@show_leaf_id = true
+      @@show_link_id = true
+      
+      @@navigation_default_classes = []
+      @@node_default_classes = []
+      @@leaf_default_classes = []
+      @@link_default_classes = []
+          
       def initialize(view_object)
         @view_object = view_object
       end
@@ -19,31 +42,43 @@ module SemanticNavigation
       
       private
       
-      def content_tag(tag_name, content = nil, options = {}, &block)
+      def tag(tag_name, object, options = {}, &block)
+        object_name = object.class.name.split('::').last.downcase
+        options ={:id => ids(object_name, object.id),
+                  :class => classes(object_name, object.active)
+                 }.merge(options)
+        content_tag(tag_name, nil, options) {yield}
+      end
+      
+      def link(object, options = {})
+        options = {:id => ids('link', object.id),
+                   :class => classes('link', object.active) 
+                  }.merge(options)
+        link_to(object.id, object.url, options)
+      end
+      
+      def content_tag(tag_name, content, options={}, &block)
         @view_object.content_tag(tag_name, content, options) {yield}
       end
       
-      def link_to(link_name, url, options = {})
+      def link_to(link_name, url, options={})
         @view_object.link_to(link_name, url, options)
       end
       
-      def navigation_classes(active = false)
-        
-      end
-      
-      def node_classes(active = false)
-        
-      end
-      
-      def leaf_classes(active = false)
-        
-      end
-      
-      def link_classes(active = false)
+      def classes(name, active)
         classes = []
-        classes.push(@@link_active_class) if active
+        classes += [send("#{name}_default_classes")].flatten
+        if active && send("show_#{name}_active_class")
+          classes.push [send("#{name}_active_class")].flatten
+        end
       end
-   
+      
+      def ids(name, id)
+        result_id = nil
+        result_id = id if send("show_#{name}_id")
+        result_id
+      end
+  
     end    
   end
 end
--- lib/semantic_navigation/renderers/list.rb
+++ lib/semantic_navigation/renderers/list.rb
@@ -2,25 +2,31 @@ module SemanticNavigation
   module Renderers
     class List < Base
       
+      @@show_navigation_active_class = false
+      @@show_node_active_class = false
+      @@show_leaf_active_class = false
+      
+      @@show_link_id = false
+      
       def render_navigation(object)
         super
-        content_tag :ul, nil, :id => object.id, :class => navigation_classes(object.active) do
+        tag :ul, object do
           object.sub_elements.sum{|element| element.render(self)}
         end
       end    
       
       def render_node(object)
-        content_tag :li, nil, :id => object.id, :class => leaf_classes(object.active) do
-          link_to(object.name, object.url, :id => object.id, :class => link_classes(object.active)) + 
-          content_tag(:ul, nil, :id => object.id, :class => node_classes(object.active)) do
+        tag :li, object do
+          link(object) + 
+          tag(:ul, object) do
             object.sub_elements.sum{|element| element.render(self)}
           end
         end 
       end
       
       def render_leaf(object)
-        content_tag :li, nil, :id => object.id, :class => leaf_classes(object.active) do
-          link_to object.name, object.url, :id => object.id, :class => link_classes(object.active)
+        tag :li, object do
+          link object
         end
       end
       
