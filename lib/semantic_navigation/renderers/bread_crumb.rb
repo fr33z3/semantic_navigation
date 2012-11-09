39c0263e5ba667d45e3ffd0a335bfe3e661359b4
Sergey Gribovski <sgribovski@gmail.com>
2012-11-09 17:39:59 +0400

    added html_options to list; fixed in bread_crumb

--- lib/semantic_navigation/renderers/bread_crumb.rb
+++ lib/semantic_navigation/renderers/bread_crumb.rb
@@ -26,12 +26,12 @@ module SemanticNavigation
       def node(object)
         content_tag(:li, nil, {:id => show_id(:leaf, object.id),
                                :class => merge_classes(:leaf, object.active, object.classes)
-                              }.merge(object.node_html)) do
+                              }.merge(object.html)) do
           link_to(object_name(object), 
                   object.url, 
                   {:id => show_id(:link, object.id),
                    :class => merge_classes(:link, object.active, object.link_classes)
-                  }.merge(object.html))
+                  }.merge(object.link_html))
         end +
         content_tag(:li) do
           breadcrumb_separator
--- lib/semantic_navigation/renderers/list.rb
+++ lib/semantic_navigation/renderers/list.rb
@@ -9,33 +9,43 @@ module SemanticNavigation
       private
 
       def navigation(object)
-        content_tag :ul, nil, :id => show_id(:navigation, object.id),
-                              :class => merge_classes(:navigation, object.active, object.classes) do
+        content_tag :ul, nil, {:id => show_id(:navigation, object.id),
+                              :class => merge_classes(:navigation, object.active, object.classes)
+                              }.merge(object.html) do
           yield
         end
       end
 
       def node(object)
-        content_tag :li, nil, :id => show_id(:leaf, object.id),
-                              :class => merge_classes(:leaf, object.active, object.classes) do
-          link_to(object_name(object), object.url, :id => show_id(:link, object.id),
-                                           :class => merge_classes(:link, object.active, object.link_classes))+
+        content_tag :li, nil, {:id => show_id(:leaf, object.id),
+                              :class => merge_classes(:leaf, object.active, object.classes)
+                              }.merge(object.html) do
+          link_to(object_name(object), 
+                  object.url, 
+                  {:id => show_id(:link, object.id),
+                   :class => merge_classes(:link, object.active, object.link_classes)
+                  }.merge(object.link_html))+
           yield
         end
       end
 
       def node_content(object)
-        content_tag(:ul, nil, :id => show_id(:node, object.id),
-                              :class => merge_classes(:node, object.active, object.node_classes)) do
+        content_tag(:ul, nil, {:id => show_id(:node, object.id),
+                               :class => merge_classes(:node, object.active, object.node_classes)
+                              }.merge(object.node_html)) do
           yield
         end
       end
 
       def leaf(object)
-        content_tag :li, nil, :id => show_id(:leaf, object.id),
-                              :class => merge_classes(:leaf, object.active, object.classes) do
-          link_to object_name(object), object.url, :id => show_id(:link, object.id),
-                                           :class => merge_classes(:link, object.active, object.link_classes)
+        content_tag :li, nil, {:id => show_id(:leaf, object.id),
+                               :class => merge_classes(:leaf, object.active, object.classes)
+                              }.merge(object.html) do
+          link_to object_name(object), 
+                  object.url, 
+                  {:id => show_id(:link, object.id),
+                   :class => merge_classes(:link, object.active, object.link_classes)
+                  }.merge(object.link_html)
         end
       end
 
