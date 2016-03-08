ebe4c55ca84cab8cd6247abe516400c41671eb62
Sergey Gribovski <sergey.gribovski@wimdu.com>
2016-03-08 01:41:43 +0100

    fixing tabs spec

--- spec/lib/semantic_navigation/twitter_bootstrap/tabs_spec.rb
+++ spec/lib/semantic_navigation/twitter_bootstrap/tabs_spec.rb
@@ -1,75 +1,64 @@
 require 'spec_helper'
 
-describe SemanticNavigation::TwitterBootstrap::Tabs do
+RSpec.describe SemanticNavigation::TwitterBootstrap::Tabs do
+  include_context "view_object"
+  let(:configuration) { SemanticNavigation::Configuration }
 
   context :renders do
-
-    before :each do
-      class ViewObject
-        attr_accessor :output_buffer
-        include ActionView::Helpers::TagHelper
-        include SemanticNavigation::HelperMethods
-        include ActionView::Helpers::UrlHelper
+    before do
+      configuration.run do
+        register_renderer :bootstrap_tabs, SemanticNavigation::TwitterBootstrap::Tabs
       end
-      @configuration = SemanticNavigation::Configuration
-      @configuration.register_renderer :bootstrap_tabs, SemanticNavigation::TwitterBootstrap::Tabs
-      @view_object = ViewObject.new
     end
 
     it 'empty ul tag for empty navigation' do
-
-      @configuration.run do
+      configuration.run do
         navigate :menu do
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == "<ul class=\"nav nav-tabs  pull-left\"></ul>"
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to eq "<ul class=\"nav nav-tabs  pull-left\"></ul>"
     end
 
     it 'one level navigation' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1'
           item :url2, 'url2', :name => 'url2'
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li>",
-                            "<a href=\"url1\">",
-                              "url1",
-                            "</a>",
-                          "</li>",
-                          "<li>",
-                            "<a href=\"url2\">",
-                              "url2",
-                            "</a>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li") do
+          with_tag("a", with: { href: "url1" }, text: "url1")
+        end
+        with_tag("li") do
+          with_tag("a", with: { href: "url2" }, text: "url2")
+        end
+      end
     end
 
     it 'icon' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1', :ico => 'user'
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li>",
-                            "<a href=\"url1\">",
-                              "<i class=\"icon-user\"></i>",
-                              "url1",
-                            "</a>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li") do
+          with_tag("a", with: { href: "url1" }) do
+            with_tag("i", with: { class: "icon-user" })
+          end
+        end
+      end
     end
 
     it 'icon in node' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1', :ico => 'user' do
             item :url, 'url', :name => 'url'
@@ -77,27 +66,24 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">",
-                              "<i class=\"icon-user\">",
-                              "</i>url1",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"url\">url",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", class: "dropdown-toggle", "data-toggle" => "dropdown", href: "#") do
+            with_tag("i", with: { class: "icon-user" })
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "url" }, text: "url")
+            end
+          end
+        end
+      end
     end
 
     it 'one multilevel navigation' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1'
@@ -108,39 +94,33 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">",
-                              "url1",
-                              "<b class=\"caret\"></b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"suburl1\">",
-                                "suburl1",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">url2",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"suburl2\">",
-                                "suburl2",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { class: "dropdown-toggle", "data-toggle" => "dropdown", href: "#" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "suburl1" }, text: "suburl1")
+            end
+          end
+        end
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { class: "dropdown-toggle", "data-toggle" => "dropdown", href: "#" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "suburl2" }, text: "suburl2")
+            end
+          end
+        end
+      end
     end
 
     it 'only root level' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1'
@@ -151,23 +131,19 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :level => 0, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li>",
-                            "<a href=\"url1\">",
-                              "url1",
-                            "</a>",
-                          "</li>",
-                          "<li>",
-                            "<a href=\"url2\">",
-                              "url2",
-                            "</a>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, level: 0, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li") do
+          with_tag("a", with: { href: "url1" }, text: "url1")
+        end
+        with_tag("li") do
+          with_tag("a", with: { href: "url2" }, text: "url2")
+        end
+      end
     end
 
     it 'second level if some item of first level is active' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1'
@@ -178,10 +154,10 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      @view_object.should_receive(:current_page?).and_return(false, true, false, false)
+      allow(view_object).to receive(:current_page?).and_return(false, true, false, false)
 
-      result = @view_object.navigation_for :menu, :level => 1, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs active  pull-left\">",
+      result = view_object.navigation_for :menu, level: 1, as: :bootstrap_tabs
+      expect(result).to eq ["<ul class=\"nav nav-tabs active  pull-left\">",
                           "<li>",
                             "<a href=\"suburl1\">suburl1",
                             "</a>",
@@ -190,7 +166,7 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
     end
 
     it 'the exact levels' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1' do
@@ -205,37 +181,33 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :levels => 0..1, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">url1",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"suburl1\">suburl1",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">url2",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"suburl2\">suburl2",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, levels: 0..1, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { href: "#", class: "dropdown-toggle", "data-toggle" => "dropdown" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "suburl1" }, text: "suburl1")
+            end
+          end
+        end
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { href: "#", class: "dropdown-toggle", "data-toggle" => "dropdown" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "suburl2" }, text: "suburl2")
+            end
+          end
+        end
+      end
     end
 
     it 'navigation except some item' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1'
@@ -246,25 +218,23 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :except_for => [:url1], :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">url2",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                              "<li>",
-                                "<a href=\"suburl2\">suburl2",
-                                "</a>",
-                              "</li>",
-                            "</ul>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, :except_for => [:url1], as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { class: "dropdown-toggle", "data-toggle" => "dropdown", href: "#" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" }) do
+            with_tag("li") do
+              with_tag("a", with: { href: "suburl2" }, text: "suburl2")
+            end
+          end
+        end
+      end
     end
 
     it 'navigation except some items' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1' do
             item :suburl1, 'suburl1', :name => 'suburl1'
@@ -275,38 +245,32 @@ describe SemanticNavigation::TwitterBootstrap::Tabs do
         end
       end
 
-      result = @view_object.navigation_for :menu, :except_for => [:suburl1,:url2], :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li class=\" dropdown\">",
-                            "<a class=\" dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">url1",
-                              "<b class=\"caret\">",
-                              "</b>",
-                            "</a>",
-                            "<ul class=\" dropdown-menu\">",
-                            "</ul>",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, :except_for => [:suburl1,:url2], as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li", with: { class: "dropdown" }) do
+          with_tag("a", with: { class: "dropdown-toggle", "data-toggle" => "dropdown", href: "#" }) do
+            with_tag("b", with: { class: "caret" })
+          end
+          with_tag("ul", with: { class: "dropdown-menu" })
+        end
+      end
     end
 
     it 'navigation only item name if url is nil' do
-      @configuration.run do
+      configuration.run do
         navigate :menu do
           item :url1, 'url1', :name => 'url1'
           item :url2, nil, :name => 'url2'
         end
       end
 
-      result = @view_object.navigation_for :menu, :as => :bootstrap_tabs
-      result.should == ["<ul class=\"nav nav-tabs  pull-left\">",
-                          "<li>",
-                            "<a href=\"url1\">",
-                              "url1",
-                            "</a>",
-                          "</li>",
-                          "<li>",
-                            "url2",
-                          "</li>",
-                        "</ul>"].join
+      result = view_object.navigation_for :menu, as: :bootstrap_tabs
+      expect(result).to have_tag("ul", with: { class: "nav nav-tabs pull-left" }) do
+        with_tag("li") do
+          with_tag("a", with: { href: "url1" }, text: "url1")
+        end
+        with_tag("li", text: "url2")
+      end
     end
 
   end
