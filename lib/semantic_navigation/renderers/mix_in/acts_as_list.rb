adf4d8b30a40485ab8aa635a8963b5861c71f882
Sergey Gribovski <sergey.gribovski@wimdu.com>
2016-03-08 12:14:29 +0100

    refactor acts_as_list

--- lib/semantic_navigation/renderers/mix_in/acts_as_list.rb
+++ lib/semantic_navigation/renderers/mix_in/acts_as_list.rb
@@ -6,24 +6,18 @@ module SemanticNavigation
         def render_navigation(object)
           return '' unless object.render_if
           navigation(object) do
-            while !object.class.in?([SemanticNavigation::Core::Leaf, NilClass]) &&
-                  from_level.to_i > object.level
-              object = object.sub_elements.find{|e| e.active}
-            end
-            show = !until_level.nil? && !object.nil? ? object.level <= until_level : true
-            if !object.class.in?([SemanticNavigation::Core::Leaf, NilClass]) && show
+            object = first_rendering_object(object)
+            if !is_leaf?(object) && show_object?(object)
               object.sub_elements.map{|element| element.render(self)}.compact.sum
             end
           end
         end
 
         def render_node(object)
-          if !object.id.in?([except_for].flatten) && object.render_if
+          if !is_exception?(object) && object.render_if
             content = render_node_content(object)
             if content
-              node(object) do
-                content
-              end
+              node(object) { content }
             else
               render_leaf(object)
             end
@@ -39,10 +33,30 @@ module SemanticNavigation
         end
 
         def render_leaf(object)
-          if !object.id.in?([except_for].flatten) && object.render_if
-            leaf(object)
-          end
+          leaf(object) if !is_exception?(object) && object.render_if
         end
+
+        private
+
+        def is_exception?(object)
+          object.id.in?([except_for].flatten)
+        end
+
+        def first_rendering_object(object)
+          while !is_leaf?(object) && from_level.to_i > object.level
+            object = object.sub_elements.find{|e| e.active}
+          end
+          object
+        end
+
+        def is_leaf?(object)
+          object.class.in?([SemanticNavigation::Core::Leaf, NilClass])
+        end
+
+        def show_object?(object)
+          until_level && object ? object.level <= until_level : true
+        end
+
       end
     end
   end
