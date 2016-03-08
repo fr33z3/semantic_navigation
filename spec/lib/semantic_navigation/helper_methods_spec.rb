402181a0f2251538453637bce215d99bf2457271
Sergey Gribovski <sergey.gribovski@wimdu.com>
2016-03-08 08:55:12 +0100

    refactoring helper methods spec

--- spec/lib/semantic_navigation/helper_methods_spec.rb
+++ spec/lib/semantic_navigation/helper_methods_spec.rb
@@ -1,86 +1,75 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe SemanticNavigation::HelperMethods do
-  before :each do
-    class ViewObject
-      include SemanticNavigation::HelperMethods
-    end
-    @view_instance = ViewObject.new
-  end
+RSpec.describe SemanticNavigation::HelperMethods do
+  include_context "view_object"
 
   describe "#navigation_for" do
+    let(:config_instance) { double }
 
-    before :each do
-      allow(SemanticNavigation::Configuration).to receive(:new).and_return (@config_instance = double)
+    before do
+      allow(SemanticNavigation::Configuration).to receive(:new).and_return config_instance
     end
 
-    it 'default renderer is :list' do
-      @config_instance.should_receive(:render).with(:some_menu, :list, {}, @view_instance)
-      @view_instance.navigation_for :some_menu
+    it "default renderer is :list" do
+      expect(config_instance).to receive(:render).with(:some_menu, :list, {}, view_object)
+      view_object.navigation_for :some_menu
     end
 
     context :sends do
-
-      it 'defined renderer name' do
-        @config_instance.should_receive(:render).with(:some_menu, :some_renderer, {}, @view_instance)
-        @view_instance.navigation_for :some_menu, :as => :some_renderer
+      it "defined renderer name" do
+        expect(config_instance).to receive(:render).with(:some_menu, :some_renderer, {}, view_object)
+        view_object.navigation_for :some_menu, as: :some_renderer
       end
 
-      it 'received properties' do
-        @config_instance.should_receive(:render).with(:some_menu, :some_renderer, {:level => 1, :except_for => :some_id}, @view_instance)
-        @view_instance.navigation_for :some_menu, :as => :some_renderer, :level => 1, :except_for => :some_id
+      it "received properties" do
+        expect(config_instance).to receive(:render).with(:some_menu, :some_renderer, { level: 1, except_for: :some_id }, view_object)
+        view_object.navigation_for :some_menu, as: :some_renderer, level: 1, except_for: :some_id
       end
     end
   end
 
   describe "#active_item_for" do
-
     context :returns do
-
-      it 'empty string if navigation is empty from items' do
+      it "empty string if navigation is empty from items" do
         SemanticNavigation::Configuration.navigate :some_menu do
         end
-        result = @view_instance.active_item_for :some_menu
-        result.should be_empty
+        expect(view_object.active_item_for :some_menu).to be_empty
       end
 
-      it 'name for an active item' do
+      it "name for an active item" do
         SemanticNavigation::Configuration.navigate :some_menu do
-          item :first, '/first', :name => 'First'
-          item :second, '/second', :name => 'Second'
+          item :first, "/first", name: "First"
+          item :second, "/second", name: "Second"
         end
-        @view_instance.should_receive(:current_page?).with('/first').and_return false
-        @view_instance.should_receive(:current_page?).with('/second').and_return true
-        result = @view_instance.active_item_for :some_menu
-        result.should == 'Second'
+        allow(view_object).to receive(:current_page?).with("/first").and_return false
+        allow(view_object).to receive(:current_page?).with("/second").and_return true
+        expect(view_object.active_item_for :some_menu).to eq "Second"
       end
 
-      it 'name for a last level' do
+      it "name for a last level" do
         SemanticNavigation::Configuration.navigate :some_menu do
-          item :first_node, '/first_node', :name => 'First node' do
-            item :first, '/first', :name => 'First'
+          item :first_node, "/first_node", name: "First node" do
+            item :first, "/first", name: "First"
           end
-          item :second_node, 'second_node', :name => 'Second node' do
-            item :second, '/second', :name => 'Second'
+          item :second_node, "/second_node", name: "Second node" do
+            item :second, "/second", name: "Second"
           end
         end
-        @view_instance.should_receive(:current_page?).exactly(4).times.and_return(false, false,true,false) # First, First node, Second, Second node
-        result = @view_instance.active_item_for :some_menu
-        result.should == 'Second'
+        allow(view_object).to receive(:current_page?).exactly(4).times.and_return(false, false,true,false)
+        expect(view_object.active_item_for :some_menu).to eq "Second"
       end
 
-      it 'name for a requested level' do
+      it "name for a requested level" do
         SemanticNavigation::Configuration.navigate :some_menu do
-          item :first_node, '/first_node', :name => 'First node' do
-            item :first, '/first', :name => 'First'
+          item :first_node, "/first_node", name: "First node" do
+            item :first, "/first", name: "First"
           end
-          item :second_node, 'second_node', :name => 'Second node' do
-            item :second, '/second', :name => 'Second'
+          item :second_node, "second_node", name: "Second node" do
+            item :second, "/second", name: "Second"
           end
         end
-        @view_instance.should_receive(:current_page?).exactly(4).times.and_return(false, false,true,false) # First, First node, Second, Second node
-        result = @view_instance.active_item_for :some_menu, 1
-        result.should == 'Second node'
+        allow(view_object).to receive(:current_page?).exactly(4).times.and_return(false, false,true,false)
+        expect(view_object.active_item_for :some_menu, 1).to eq "Second node"
       end
     end
   end
